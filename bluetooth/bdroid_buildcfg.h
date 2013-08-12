52a96255d2514ab4d7ddfd55560cdf032b34c9fb
jt1134 <jt1134@gmail.com>
2013-08-11 21:57:41 -0500

    expressatt: update bluetooth configuration

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Express"
+#define BTM_DEF_LOCAL_NAME	"Galaxy Express"
+#define BTA_DISABLE_DELAY 1000	/* in milliseconds */
 
 #endif
