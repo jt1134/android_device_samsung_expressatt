c3eecaff1410566b4a980c2b9cd1cf2a6c895b87
jt1134 <jt1134@gmail.com>
2013-09-01 03:30:00 -0500

    expressatt: add build flags and recovery.fstab for TWRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,3 +69,29 @@ BOARD_USE_LEGACY_SENSORS_FUSION := false
 
 # Backlight notification
 BOARD_USE_BLN := true
+
+# TWRP
+ifeq ($(BUILD_TWRP), true)
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p15"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "discard,noauto_da_alloc,journal_async_commit"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+
+TARGET_RECOVERY_FSTAB := device/samsung/expressatt/recovery.fstab
+endif
