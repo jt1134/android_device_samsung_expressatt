fe570657fe14b7bff580575c22e2e4dd215f9a5f
Dave Daynard <nardholio@gmail.com>
2014-03-18 19:42:13 -0400

    Apexq: update BLE config (with thanks to Arco)
    Change-Id: Iad9a44a6f87dda532a03565d991ec7d0771020b2

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,5 +22,9 @@
 #define BTA_DISABLE_DELAY 1000	/* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BTA_HOST_INTERLEAVE_SEARCH TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
 
 #endif
