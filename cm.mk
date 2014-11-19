54a8d3e1807876fc108b5af54d00cee84de902f6
jt1134 <jt1134@gmail.com>
2014-11-19 02:27:30 -0800

    expressatt: remove call to missing makefile

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/expressatt/full_expressatt.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
